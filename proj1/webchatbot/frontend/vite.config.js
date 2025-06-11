54c78238336a1e3b8f768d6fc9bc9507f4d3dc61
Amirlan <amirlanabd2@gmail.com>
2025-06-12 02:24:52 +0500

    config

--- proj1/webchatbot/frontend/vite.config.js
+++ proj1/webchatbot/frontend/vite.config.js
@@ -1,14 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// frontend/vite.config.js
-export default {
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       '/chat': 'http://localhost:8000',
       '/teach': 'http://localhost:8000',
       '/register': 'http://localhost:8000',
-      '/login': 'http://localhost:8000',
-    },
-  },
-}
+      '/login': 'http://localhost:8000'
+    }
+  }
+});
